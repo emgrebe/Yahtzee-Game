30baf0694e847b3e24bb6e95cdf3dc84fd54e8ed
Emily Landgrebe <emilylandgrebe@Emilys-MacBook-Pro.local>
2019-07-30 18:40:09 -0500

    working base

---
+++ js/main.js
@@ -0,0 +1,48 @@
+/*----- constants -----*/ 
+const faceDie = [
+    {
+        face: 1,
+        dieImage: 'images/dice1.png'   
+    },
+    {
+        face: 2,
+        dieImage: 'images/dice2.png'
+    },
+    {
+        face: 3,
+        dieImage: 'images/dice3.png'
+    },
+    {
+        face: 4,
+        dieImage: 'images/dice4.png'
+    },
+    {
+        face: 5,
+        dieImage: 'images/dice5.png'
+    },
+    {
+        face: 6,
+        dieImage: 'images/dice6.png'
+    }
+]
+
+const COMBOS = [
+    [1, 1 ],
+    [1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+
+]
+
+/*----- app's state (variables) -----*/
+l
+
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/ 
+
+
+/*----- functions -----*/
