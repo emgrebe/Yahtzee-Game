6956816110ee26ea4856bab1ddc3c48e984aefa1
Emily Landgrebe <emilylandgrebe@emilys-mbp.lan>
2019-08-01 22:32:46 -0500

    problem solving javascript

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 /*----- constants -----*/ 
 var button = document.getElementById('rollButton');
-var totalGames = 0;
 let die = [1, 1, 1, 1];
 const faces = 6; //min: 0; max:6;
 const min = 1; // min of the value of the faces
 const max = 6; // max of the value of the faces
-const heldValues = [false, false, false, false];
-const currentTurn = 0;
-const maxTurn = 12;
+const maxRounds = 12;
 const currentRoll = 0;
 const MAXROLLS = 3;
 const faceImg = [
@@ -37,64 +34,65 @@ const faceImg = [
     }
 ]
 
-const ones = [
-    [1, 1 ], // 2
-    [1, 1, 1],//3
-    [1, 1, 1, 1] // 4
-]
+// const ones = [
+//     [1, 1 ], // 2
+//     [1, 1, 1],//3
+//     [1, 1, 1, 1] // 4
+// ]
 
-const twos = [
-    [2, 2], // 4
-    [2, 2, 2], // 6
-    [2, 2, 2, 2] // 8
-]
+// const twos = [
+//     [2, 2], // 4
+//     [2, 2, 2], // 6
+//     [2, 2, 2, 2] // 8
+// ]
 
-const threes = [
-    [3, 3], //  6
-    [3, 3, 3], // 9
-    [3, 3, 3, 3] //12
-]
+// const threes = [
+//     [3, 3], //  6
+//     [3, 3, 3], // 9
+//     [3, 3, 3, 3] //12
+// ]
 
-const fours = [
-    [4, 4], // 8
-    [4, 4, 4], // 12
-    [4, 4, 4, 4] // 16
-]
-const fives = [
-    [5, 5], // 10
-    [5, 5, 5], // 15
-    [5, 5, 5, 5] // 20
-]
-const sixes = [
-    [6, 6], // 12
-    [6, 6, 6],// 18
-    [6, 6, 6, 6] // 24
-]
+// const fours = [
+//     [4, 4], // 8
+//     [4, 4, 4], // 12
+//     [4, 4, 4, 4] // 16
+// ]
 
-const threeOfAKind = [
-    [1, 1, 1], // 3
-    [2, 2, 2], // 6
-    [3, 3, 3], // 9
-    [4, 4, 4], // 12
-    [5, 5, 5], //15
-    [6, 6, 6], // 18
-]
+// const fives = [
+//     [5, 5], // 10
+//     [5, 5, 5], // 15
+//     [5, 5, 5, 5] // 20
+// ]
 
-const largeStraight = [
-    [1, 2, 3, 4],
-    [2, 3, 4, 5],
-    [3, 4, 5, 6]
-] // score = 30
+// const sixes = [
+//     [6, 6], // 12
+//     [6, 6, 6],// 18
+//     [6, 6, 6, 6] // 24
+// ]
 
-const yahtzee = [
-    [1, 1, 1, 1],
-    [2, 2, 2, 2],
-    [3, 3, 3, 3],
-    [4, 4, 4, 4],
-    [5, 5, 5, 5],
-    [6, 6, 6, 6]
-] // score = 50
+// const threeOfAKind = [
+//     [1, 1, 1], // 3
+//     [2, 2, 2], // 6
+//     [3, 3, 3], // 9
+//     [4, 4, 4], // 12
+//     [5, 5, 5], //15
+//     [6, 6, 6], // 18
+// ]
 
+// const largeStraight = [
+//     [1, 2, 3, 4],
+//     [2, 3, 4, 5],
+//     [3, 4, 5, 6]
+// ] // score = 30
+
+// const yahtzee = [
+//     [1, 1, 1, 1],
+//     [2, 2, 2, 2],
+//     [3, 3, 3, 3],
+//     [4, 4, 4, 4],
+//     [5, 5, 5, 5],
+//     [6, 6, 6, 6]
+// ] // score = 50
 
 /*----- app's state (variables) -----*/
 let board, turn;
@@ -107,17 +105,17 @@ let board, turn;
 
 
 /*----- functions -----*/
-
 function dice() {
     die.forEach((el, idx) => {
         let num =  Math.floor(Math.random() * 6) + 1;
         die[idx] = num
+        console.log(num);
       })
    render()
 }
 
 button.onclick = function() {
-    dice()
+    dice();
 }
 
 function render() {
@@ -132,13 +130,39 @@ function render() {
 
 render();
 
+// preload the six images first
+// var face0=new Image()
+// face0.src="d1.gif"
+// var face1=new Image()
+// face1.src="d2.gif"
+// var face2=new Image()
+// face2.src="d3.gif"
+// var face3=new Image()
+// face3.src="d4.gif"
+// var face4=new Image()
+// face4.src="d5.gif"
+// var face5=new Image()
+// face5.src="d6.gif"
+// </script>
 
-function rollDice() {
-    var dieId = this.getAttribute('data-id');
-    this.setAttribute('src', die[dieId].dieImage);
-    console.log(die[dieId].dieImage)
-    rollCount++;
-}
+
+// <img src="d1.gif" name="mydice">
+// <form>
+// <input type="button" value="Throw dice!" onClick="throwdice()">
+// </form>
+// <script>
+// function throwdice(){
+// //create a random integer between 0 and 5
+// var randomdice=Math.round(Math.random()*5)
+// document.images["mydice"].src=eval("face"+randomdice+".src")
+// }
+
+{/* // function rollDice() { */}
+//     var dieId = this.getAttribute('data-id');
+//     this.setAttribute('src', die[dieId].dieImage);
+//     console.log(die[dieId].dieImage)
+//     rollCount++;
+// }
 
 // function createScoreBoard()
 // function createDice()
