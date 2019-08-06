057d66a48dd4b9a259fc3b4cb83df73d4a08f1a9
Emily Landgrebe <emilylandgrebe@Emilys-MacBook-Pro.local>
2019-08-06 12:52:46 -0500

    matched the values of the vace of each die to the picture

--- js/main.js
+++ js/main.js
@@ -2,36 +2,45 @@
 const button = document.getElementById('rollButton');
 let die = [1, 1, 1, 1];
 const maxRounds = 12;
-const currentRoll = 0;
+let currentRoll = 0;
 const MAXROLLS = 3;
 let arr = [];
+let sum = 0;
+
 const faceImg = [
     {
         face: '1',
-        dieImage: 'images/dice1.png'   
+        dieImage: 'images/dice1.png',
+        isHolding: false  //on click flip to true
     },
     {
         face: '2',
-        dieImage: 'images/dice2.png'
+        dieImage: 'images/dice2.png',
+        isHolding: false
     },
     {
         face: '3',
-        dieImage: 'images/dice3.png'
+        dieImage: 'images/dice3.png',
+        isHolding: false
     },
     {
         face: '4',
-        dieImage: 'images/dice4.png'
+        dieImage: 'images/dice4.png',
+        isHolding: false
     },
     {
         face: '5',
-        dieImage: 'images/dice5.png'
+        dieImage: 'images/dice5.png',
+        isHolding: false
     },
     {
         face: '6',
-        dieImage: 'images/dice6.png'
+        dieImage: 'images/dice6.png',
+        isHolding: false
     }
 ]
-
+// console.log(faceImg)
+// console.log(faceImg[2].isHolding)
 const ones = [
     [1, 1 ], // 2
     [1, 1, 1],//3
@@ -105,9 +114,25 @@ let board, scores;
 function rollDice() {
     die.forEach((el, idx) => {
         let num =  Math.floor(Math.random() * 6) + 1;
+        let imgValue = faceImg[idx].face
         die[idx] = num
-        arr.push(num);
+        imgValue = num
+        arr.push(imgValue)
     })
+
+    // let savedValues = die.map(faceImg => {
+    //     if(faceImg.isHolding === true) {
+    //         let imgval = faceImg[savedValues].face    
+    //         arr.push(face) = savedValues
+    //         arr = savedValues
+    //         return imgval
+    //     } else {
+    //         return;
+    //     }
+    // })
+    // console.log(savedValues)
+   
+    sumArray()
     render()
 };
 
@@ -118,31 +143,49 @@ button.onclick = function() {
 function render() {
     let dieContainer = document.querySelectorAll('.dice')
     if(dieContainer.length === 0){
-    die.forEach((el, idx) => {
-        let imgElement = document.createElement('img');
-        imgElement.setAttribute('src', `images/dice${el}.png`);
-        imgElement.setAttribute('id', `${idx}`);
-        imgElement.setAttribute('class', 'dice');
-        imgElement.addEventListener('click', die);
-        document.querySelector('.die').appendChild(imgElement);
-    })
+        die.forEach((el, idx) => {
+            let imgElement = document.createElement('img');
+            imgElement.setAttribute('src', `images/dice${el}.png`);
+            imgElement.setAttribute('id', `${idx}`);
+            imgElement.setAttribute('class', 'dice');
+            imgElement.addEventListener('click', die);
+            document.querySelector('.die').appendChild(imgElement);
+            let inptBtn = document.createElement('input');
+            inptBtn.setAttribute('type', 'checkbox')
+            inptBtn.setAttribute('id', 'chck')
+            inptBtn.setAttribute('value', `${el}`)
+            document.querySelector('.die').appendChild(inptBtn)
+        })
     } else {
         die.forEach((el, idx) => {
-        let indv = document.getElementById(`${idx}`);   
-        indv.setAttribute('src', `images/dice${el}.png`)
+            let indv = document.getElementById(`${idx}`);   
+            indv.setAttribute('src', `images/dice${el}.png`)
+            indv.setAttribute('value', `${el}`)
+
+        //     let inputContainer = document.getElementById('input')
+        // inputContainer[el] = indv[el, idx]
+    console.log('hi')
         })
     }  
  };
-    
-render();
-
 
 function sumArray() {
-    var sum = 0;
-    for(var i = 0; i < arr.length; i ++) {
-        sum += arr[i];
+    for(let i = 0; i < die.length; i ++) {
+        sum += die[i];
     }
-    return [sum];
+    document.getElementById('sub-result').innerText = sum;
+    sum = 0;
 };
 
 render();
+
+let isHolding = document.querySelector('input')
+isHolding.addEventListener('click', function() {
+    toggleCheckbox()
+})
+
+function toggleCheckbox() {
+    if(isHolding === false) {
+        console.log('hit')
+    }
+}
