bd8acf80ead380e9c071ea730b2e0533a35d3ce4
Emily Landgrebe <emilylandgrebe@Emilys-MacBook-Pro.local>
2019-08-07 10:37:39 -0500

    checkbox value mirrors facevalue

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let currentRoll = 0;
 const MAXROLLS = 3;
 let arr = [];
 let sum = 0;
-
+let held = [false, false, false, false];
 const faceImg = [
     {
         face: '1',
@@ -111,18 +111,26 @@ let board, scores;
 /*----- event listeners -----*/ 
 
 
-let checkBox0 = document.getElementById('c-0')
-let checkBox1 = document.getElementById('c-1')
-let checkBox2 = document.getElementById('c-2')
-let checkBox3 = document.getElementById('c-3')
-console.log(checkBox1)
+// let checkBox0 = document.getElementById('c-0')
+// let checkBox1 = document.getElementById('c-1')
+// let checkBox2 = document.getElementById('c-2')
+// let checkBox3 = document.getElementById('c-3')
+// console.log(checkBox1)
 
-let allEl = document.querySelectorAll('input')
-console.log(allEl)
+// let allEl = document.querySelectorAll('input')
+// console.log(inptBtn)
 
 /*----- functions -----*/
 function rollDice() {
     die.forEach((el, idx) => {
+        if(currentRoll < 3) {
+			for(var i = 0; i<4; i++) {
+				if(held[i] === false || currentRoll === 0) {
+					die[i].innerHTML = Math.floor(Math.random() * 6) + 1;
+				}
+			}
+			currentRoll++;
+		}
         let num =  Math.floor(Math.random() * 6) + 1;
         let imgValue = faceImg[idx].face
         die[idx] = num
@@ -134,7 +142,7 @@ function rollDice() {
 };
 
 button.onclick = function() {
-    rollDice();
+    rollDice()
 };
 
 function render() {
@@ -142,51 +150,73 @@ function render() {
     if(dieContainer.length === 0){
         die.forEach((el, idx) => {
             let imgElement = document.createElement('img');
-            imgElement.setAttribute('src', `images/dice${el}.png`);
-            imgElement.setAttribute('id', `${idx}`);
-            imgElement.setAttribute('class', 'dice');
-            imgElement.addEventListener('click', die);
-            document.querySelector('.die').appendChild(imgElement);
-            // inptBtn.addEventListener('click', die)
-            inptBtn = document.createElement('input');
+            imgElement.setAttribute('src', `images/dice${el}.png`)
+            imgElement.setAttribute('id', `${idx}`)
+            imgElement.setAttribute('class', 'dice')
+            imgElement.addEventListener('click', rollDice)
+            document.querySelector('.die').appendChild(imgElement)
+            inptBtn = document.createElement('input')
             inptBtn.setAttribute('type', 'checkbox')
             inptBtn.setAttribute('class', 'chck')
             inptBtn.setAttribute('id', `c-${idx}`)
             inptBtn.setAttribute('value', `${el}`)
             document.querySelector('.die').appendChild(inptBtn)
         })
-        console.log(inptBtn)
     } else {
         die.forEach((el, idx) => {
-            let indv = document.getElementById(`${idx}`);   
+            let indv = document.getElementById(`${idx}`)
             indv.setAttribute('src', `images/dice${el}.png`)
             indv.setAttribute('value', `${el}`)
-           
+            let checkBtn = document.getElementById(`c-${idx}`)
+            checkBtn.setAttribute('value', `${el}`)
         })
     }  
  };
 
-function sumArray() {
-    for(let i = 0; i < die.length; i ++) {
-        sum += die[i];
+function updateHold() {
+    for(var i = 0; i < 4; i++) {
+        held[i] = document.getElementById(`c-${idx}`).checked
     }
-    document.getElementById('sub-result').innerText = sum;
-    sum = 0;
 };
 
-render();
+function sumArray() {
+    for(let i = 0; i < die.length; i ++) {
+        sum += die[i]
+    }
+    document.getElementById('sub-result').innerText = sum
+    sum = 0;
+    updateHold()
+};
+
+render()
 
 let isHolding = document.querySelectorAll('input')
 isHolding.onclick =  function() {
-    check()
 }
 
 function check() {
     if(isHolding = true) {       
     }
-}
+};
 
 function uncheck() {
     if(isHolding = false) {
     }
-}
+};
+
+// function upperScore (value) {
+//     let accumlator = 0;
+//     for(var i = 0 ; i < 5; i++) {
+//         if(Number(die[i].innerHTML) === value) {
+//             accumulator = accumlator + value
+//         }
+//         return accumlator
+//     }
+// }
+
+// getElm('one').onclick = upperScore('one', 1)
+// getElm('two').onclick = upperScore('two', 2)
+// getElm('three').onclick = upperScore('three', 3)
+// getElm('four').onclick = upperScore('ones', 4)
+// getElm('five').onclick = upperScore('ones', 5)
+// getElm('six').onclick = upperScore('ones', 6)
